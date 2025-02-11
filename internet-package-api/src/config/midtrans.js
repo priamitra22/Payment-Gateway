d5822119da0d74e3d51c2b60d3fdaa7b15eafbdf
priamitra22 <gymnoini@gmail.com>
2025-02-11 11:49:03 +0700

    Remove secret from .env

--- internet-package-api/src/config/midtrans.js
+++ internet-package-api/src/config/midtrans.js
@@ -2,8 +2,8 @@ const midtransClient = require("midtrans-client");
 
 const snap = new midtransClient.Snap({
   isProduction: false, // Gunakan false untuk sandbox mode
-  serverKey: "SB-Mid-server-mlpK9SgUCxmpsnUCHoc29k_h",
-  clientKey: "SB-Mid-client-NLDNwa2CkmKVO3rD",
+  serverKey: "",
+  clientKey: "",
 });
 
 module.exports = snap;
